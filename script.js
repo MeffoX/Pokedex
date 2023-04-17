633ac4948b5ebfc5d798ba6cfad33f7145f7b9f8
Stefan <roth.neukirchen@googlemail.com>
2023-04-17 09:41:08 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let currentPokemon;
+
+async function loadPokemon() {
+    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let response = await fetch(url);
+    currentPokemon = await response.json();
+    console.log('Loaded', currentPokemon);
+
+    renderPokemonInfo();
+}
+
+function renderPokemonInfo() {
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+    document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+}
